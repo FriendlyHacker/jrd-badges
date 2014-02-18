5eb610e919aaa1eca7f5f3fccf940d03b7cea4a2
Chad Windnagle <drmmr763@gmail.com>
2014-02-17 21:46:03 -0500

    add preview function to list view

--- admin/com_jrdbadges/views/jrdbadges/tmpl/default.php
+++ admin/com_jrdbadges/views/jrdbadges/tmpl/default.php
@@ -35,14 +35,20 @@ JHtml::_('formbehavior.chosen', 'select');
         <table class="table table-striped" id="badge-list">
             <thead>
                 <tr>
+                    <th width="1%" class="hidden-phone">
+                        <?php echo JHtml::_('grid.checkall'); ?>
+                    </th>
                     <th width="1%" style="min-width:55px" class="nowrap center">
                         <?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.published'); ?>
                     </th>
                     <th>
-                        <?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title'); ?>
+                        <?php echo JText::_('JGLOBAL_TITLE'); ?>
+                    </th>
+                    <th>
+                        <?php echo JText::_('PREVIEW'); ?>
                     </th>
                     <th width="1%" class="nowrap center hidden-phone">
-                        <?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id'); ?>
+                        <?php echo JText::_('JGRID_HEADING_ID'); ?>
                     </th>
                 </tr>
             </thead>
@@ -52,11 +58,36 @@ JHtml::_('formbehavior.chosen', 'select');
                 foreach ($this->items as $i => $item) :
             ?>
                 <tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->catid?>">
+                    <td class="center hidden-phone">
+                        <?php echo JHtml::_('grid.id', $i, $item->id); ?>
+                    </td>
                     <td>
-                        <?php echo $this->escape($item->state);  ?>
+                        <?php echo JHtml::_('jgrid.published', $item->state, $i, 'contacts.', true, 'cb'); ?>
                     </td>
                     <td>
                       <?php echo $this->escape($item->title);  ?>
+                    </td>
+                    <td>
+                        <a data-toggle="modal" data-target="#badge-<?php echo $item->id; ?>">Preview</a>
+                        <div class="modal fade" id="badge-<?php echo $item->id; ?>">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h4>Preview Badge</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <?php if (strpos($item->badge, "http://")): ?>
+                                            <img src="<?php echo $item->badge; ?>" />
+                                        <?php else: ?>
+                                            <img src="<?php echo JUri::root() . $item->badge; ?>" />
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </td>
                     <td class="center hidden-phone">
                         <?php echo $item->id; ?>
